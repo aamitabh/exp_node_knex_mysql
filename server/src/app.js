b42d9575a709ce8db0ec58722d1d0c6eed2e704a
amitabh <buddhabacchan@gmail.com>
2018-06-26 23:29:37 -0700

    changed env to dev and test

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,12 @@
 require('dotenv').config()
 
-console.log('process.env.MYDB-ADMIN-HOST:', process.env.MYDB_ADMIN_HOST);
+console.log('process.env.MYDB_DEV_HOST:', process.env.MYDB_DEV_HOST);
+console.log('process.env.MYDB_DEV_DATABASE:', process.env.MYDB_DEV_DATABASE);
+console.log('process.env.MYDB_DEV_USER:', process.env.MYDB_DEV_USER);
+console.log('process.env.MYDB_DEV_PASSWORD:', process.env.MYDB_DEV_PASSWORD);
+
+console.log('process.env.MYDB_TEST_HOST:', process.env.MYDB_TEST_HOST);
+console.log('process.env.MYDB_TEST_DATABASE:', process.env.MYDB_TEST_DATABASE);
+console.log('process.env.MYDB_TEST_USER:', process.env.MYDB_TEST_USER);
+console.log('process.env.MYDB_TEST_PASSWORD:', process.env.MYDB_TEST_PASSWORD);
+
