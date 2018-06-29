48efbb251ce01f204ae2993b73222b85e8fc903c
Arora, Amitabh <Amitabh.Arora@chevron.com>
2018-06-28 18:06:57 -0700

    conditional dotenv

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const knex = require('../db/knexfile')
 
 console.log('process.env.MYDB_DEV_HOST:', process.env.MYDB_DEV_HOST);
-// console.log('process.env.MYDB_DEV_DATABASE:', process.env.MYDB_DEV_DATABASE);
+console.log('process.env.MYDB_DEV_DATABASE:', process.env.MYDB_DEV_DATABASE);
 console.log('process.env.MYDB_DEV_USER:', process.env.MYDB_DEV_USER);
 console.log('process.env.MYDB_DEV_PASSWORD:', process.env.MYDB_DEV_PASSWORD);
 
@@ -12,4 +14,3 @@ console.log('process.env.MYDB_DEV_PASSWORD:', process.env.MYDB_DEV_PASSWORD);
 // console.log('process.env.MYDB_TEST_DATABASE:', process.env.MYDB_TEST_DATABASE);
 // console.log('process.env.MYDB_TEST_USER:', process.env.MYDB_TEST_USER);
 // console.log('process.env.MYDB_TEST_PASSWORD:', process.env.MYDB_TEST_PASSWORD);
-
